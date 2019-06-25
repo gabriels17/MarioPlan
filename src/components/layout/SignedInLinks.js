577f47a6b1496d5d2716cba8dba68a76af90d4e6
SIGVA media <sigvamedia@SIGVAs-iMac.local>
2019-06-25 01:12:39 +0000

    Added user profile data to firebase

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -3,12 +3,12 @@ import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { signOut } from '../../store/actions/authActions';
 
-const SignedInLinks = (props) => {
+const SignedInLinks = ({ profile, signOut }) => {
   return (
       <ul className="right">
         <li><NavLink to='/create'>New Project</NavLink></li>
-        <li><NavLink to='/' onClick={props.signOut}>Logout</NavLink></li>
-        <li><NavLink to='/' className="btn btn-floating pink lighten-1">GS</NavLink></li>
+        <li><NavLink to='/' onClick={signOut}>Logout</NavLink></li>
+        <li><NavLink to='/' className="btn btn-floating pink lighten-1">{profile.initials}</NavLink></li>
       </ul>
   );
 };
